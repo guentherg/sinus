9b637c67beb9ab369cc888510145e2099b14a9fe
guentherg <guenthergruetzmacher@gmail.com>
2017-10-18 16:28:30 +0200

    c

--- plotter/plotter.js
+++ plotter/plotter.js
@@ -12,10 +12,10 @@ function setup() {
 	strokeWeight(1);
   	as = createSlider(0, 4, 1, 0.5);
   	bs = createSlider(0, 13, 1, 0.1);
-  	cs = createSlider(0, 4, 1, 0.1);
+  	cs = createSlider(0, (4*PI)+(PI/2), 1, PI/2);
   	as.position(300, 35);
   	bs.position(300, 55);
-	cs.position(300, 75);
+		cs.position(300, 75);
 }
 
 function draw() {
@@ -24,9 +24,10 @@ function draw() {
   	line(width/2, 0, width/2, height);
   	line(0, height/2, width, height/2);
 
-	fill(0);
+		fill(0);
   	text("a = "+String(a), 50, 50);
   	text("b = "+String(b), 50, 70);
+  	text("c = "+String(c), 50, 90);
   	text("x", width-20, height/2+20);
   	text("y", width/2+20, 20);
 
@@ -39,7 +40,7 @@ function draw() {
   		line(width/2-10, height/2+i, width/2+10, height/2+i);
   		text(String(-1*(i/100)), width/2+20, height/2+i);
   	}
-  	
+
   	for(var i = -3; i <= 3; i++){
   		line(width/2+(i*PI*100), height/2-10, width/2+(i*PI*100), height/2+10);
 		if(i != 0){
